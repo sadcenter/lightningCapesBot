a4e11f595ce4a623f155a4e6640a8edb04efa283
Szymusek07 <szymsu07@gmail.com>
2021-03-03 22:42:41 +0100

    some code improvement czy cos takiego

--- src/main/java/xyz/lightningcapes/api/handler/CommandHandler.java
+++ src/main/java/xyz/lightningcapes/api/handler/CommandHandler.java
@@ -1,6 +1,7 @@
 package xyz.lightningcapes.api.handler;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -18,23 +19,25 @@ public class CommandHandler extends ListenerAdapter {
             return;
         Message message = event.getMessage();
         String content = message.getContentRaw();
+        TextChannel channel = event.getChannel();
+        long channelId = channel.getIdLong();
         if (content.charAt(0) == LightningBot.PREFIX) {
             String[] raw = content.split(" ");
             String substring = raw[0].substring(1);
             Command command = Bootstrap.getInstance()
                     .getCommandManager().getByName(substring);
             if (command == null) {
-                if (Bootstrap.getInstance().getConfiguration().notWriteChannels.contains(event.getChannel().getIdLong())) {
+                if (Bootstrap.getInstance().getConfiguration().notWriteChannels.contains(channelId)) {
                     message.delete().queue();
                 }
                 return;
             }
 
-            if (!event.getChannel().getId().equals(String.valueOf(command.getChannelId())))
+            if (channelId != 0L && command.getChannelId() != channelId)
                 return;
 
-            command.handle(event.getMember(), message, event.getChannel(), Arrays.copyOfRange(raw, 1, raw.length));
-        } else if (Bootstrap.getInstance().getConfiguration().notWriteChannels.contains(event.getChannel().getIdLong())) {
+            command.handle(event.getMember(), message, channel, Arrays.copyOfRange(raw, 1, raw.length));
+        } else if (Bootstrap.getInstance().getConfiguration().notWriteChannels.contains(channelId)) {
             message.delete().queue();
         }
     }
